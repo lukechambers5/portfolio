5a99b662fb55e16652e96beb84d42b6bd799673e
Luke <lukechambers0217@gmail.com>
2025-07-02 16:07:21 -0500

    got rid of unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 "use client";
 import React from "react";
 import { WavyBackground } from "@/components/ui/WavyBackground";
-import { TextHoverEffect } from "@/components/ui/TextHoverEffect";
 
 export default function HomePage() {
   return (
